938f89d0be80850a0dea8705d378e85a07b0fbda
crossian123 <84463337+crossian123@users.noreply.github.com>
2021-05-29 02:13:47 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,5 +3,5 @@ import os
 pytest_plugins = ["pytester", "pytest_serverless"]
 
 os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "foobar_key")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "foobar_key1")
 os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "foobar_secret")
